37bbe0b06a2d922ff90d8ee8d2e058faa5f4bf61
Jakub Popelka <jakub.popelka@gmail.com>
2017-11-13 09:19:52 +0100

    config class

---
+++ src/Lib/Config.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace VfTest\Lib;
+
+use Symfony\Component\Yaml\Yaml;
+
+class Config {
+
+    /**
+     * @var LocationClient
+     */
+    private $configValues;
+
+    /**
+     * @constructor
+     * @param string $configFileName YAML config file name including path
+     * @return void
+     */
+    public function __construct($configFileName) {
+        $configValues = Yaml::parse(file_get_contents($configFileName));
+        $this->setConfigValues($configValues);
+    }
+
+    /**
+     * Sets config values
+     * @param array $configValues Array with config values
+     * @return void
+     */
+    public function setConfigValues($configValues) {
+        $this->configValues = $configValues;
+    }
+
+    /**
+     * Gets config value
+     * @param string $configKey Config key
+     * @return mixed
+     */
+    public function getConfigValue($configKey) {
+        return $this->configValues[$configKey];
+    }
+
+}
